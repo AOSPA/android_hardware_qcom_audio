d6650f6570b5274d69273e78f35a62247380ed3f
Ethan Chen <intervigil@gmail.com>
2015-06-07 12:26:14 -0700

    hal: Convert libaudioamp to audio_amplifier HAL
    Change-Id: I5113923e3cc1989f2272ea439735492c9ce4d8a3
    Signed-off-by: Jan Dula <frantisheq@gmail.com>

--- hal/Android.mk
+++ hal/Android.mk
@@ -137,6 +137,7 @@ endif
 LOCAL_SHARED_LIBRARIES := \
 	liblog \
 	libcutils \
+	libhardware \
 	libtinyalsa \
 	libtinycompress \
 	libaudioroute \
@@ -147,6 +148,7 @@ LOCAL_C_INCLUDES += \
 	external/tinyalsa/include \
 	external/tinycompress/include \
 	external/expat/lib \
+	hardware/libhardware/include \
 	$(call include-path-for, audio-route) \
 	$(call include-path-for, audio-effects) \
 	$(LOCAL_PATH)/$(AUDIO_PLATFORM) \
