f8978005d6424aa279250ab3a69f6b1dee9fa820
Sudheer Papothi <spapothi@codeaurora.org>
2015-10-24 03:26:38 +0530

    hal: Add performance lock during device switch
    Add performance lock during device switch in audio playback
    scenario. Acquiring performance lock will improve the device
    switch latency. Change acquires performance lock during device
    switch in audio playback scenario.
    
    Change-Id: I28b951cb7180e2786d4c5037182169cfb48ba851

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -1601,8 +1601,13 @@ static int out_set_parameters(struct audio_stream *stream, const char *kvpairs)
         if (val != 0) {
             out->devices = val;
 
-            if (!out->standby)
+            if (!out->standby) {
+                audio_extn_perf_lock_acquire(&adev->perf_lock_handle, 0,
+                                             adev->perf_lock_opts,
+                                             adev->perf_lock_opts_size);
                 select_devices(adev, out->usecase);
+                audio_extn_perf_lock_release(&adev->perf_lock_handle);
+            }
 
             if ((adev->mode == AUDIO_MODE_IN_CALL) &&
                     output_drives_call(adev, out)) {
