988a534d3bd5b56ddec44bcfcf2818bfe085e8ca
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    bengal: Removed audio A2dp from makefile
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- configs/bengal/bengal.mk
+++ configs/bengal/bengal.mk
@@ -86,7 +86,6 @@ AUDIO_FEATURE_ENABLED_BATTERY_LISTENER := true
 
 BOARD_SUPPORTS_OPENSOURCE_STHAL := true
 
-AUDIO_HARDWARE := audio.a2dp.default
 AUDIO_HARDWARE += audio.usb.default
 AUDIO_HARDWARE += audio.r_submix.default
 AUDIO_HARDWARE += audio.primary.bengal
