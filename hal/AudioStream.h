59ecbcf99496f37eb238d559f785929f5e7aaa5f
Zhou Song <quic_zhous@quicinc.com>
2022-06-22 18:25:59 +0800

    hal: add support to read all available capabilities
    Currently USB will only return default configs or
    predefined configs.
    Update to return all supported configs such as
    channel mask, sample rate, format.
    
    Change-Id: I8dac370fe494c63c82f7f5fa36e5db55c586cafb

--- hal/AudioStream.h
+++ hal/AudioStream.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -519,6 +520,7 @@ protected:
     struct pal_volume_data *volume_; /* used to cache volume */
     std::map <audio_devices_t, pal_device_id_t> mAndroidDeviceMap;
     int mmap_shared_memory_fd;
+    pal_param_device_capability_t *device_cap_query_;
 };
 
 class StreamOutPrimary : public StreamPrimary {
