91593d01f665f159fc3974379e69f1b76ca274c1
ReallySnow <reallysnow233@gmail.com>
2023-04-21 21:15:59 +0800

    configs: lito: Disable DLKM
    Change-Id: I557c346f701b772dca2bfb1758cb4b2f3e01bb73
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- configs/lito/lito.mk
+++ configs/lito/lito.mk
@@ -57,7 +57,7 @@ endif
 DOLBY_ENABLE := false
 endif
 
-AUDIO_FEATURE_ENABLED_DLKM := true
+AUDIO_FEATURE_ENABLED_DLKM := false
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 BOARD_SUPPORTS_GCS := false
 AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
@@ -142,7 +142,6 @@ BOARD_VENDOR_KERNEL_MODULES := \
     $(KERNEL_MODULES_OUT)/audio_native.ko \
     $(KERNEL_MODULES_OUT)/audio_machine_lito.ko \
     $(KERNEL_MODULES_OUT)/audio_snd_event.ko
-endif
 
 #Audio DLKM
 AUDIO_DLKM := audio_apr.ko
@@ -176,6 +175,7 @@ AUDIO_DLKM += audio_machine_lito.ko
 AUDIO_DLKM += audio_snd_event.ko
 
 PRODUCT_PACKAGES += $(AUDIO_DLKM)
+endif
 
 ifneq ($(strip $(TARGET_USES_RRO)), true)
 #Audio Specific device overlays
