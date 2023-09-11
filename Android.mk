1c4e14e2d3fa50c966d0db2501905c10a63cfca8
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-11 02:29:33 +0000

    Revert "audio: Don't include any makefiles if BSP is not present"
    This reverts commit 6801de202d45edd0bce1ee95d1a270d05dbf002d.
    
    Change-Id: Id3590e961f13516443da4de972210cd7a25a603d

--- Android.mk
+++ Android.mk
@@ -3,11 +3,9 @@ ifeq ($(TARGET_USES_QCOM_MM_AUDIO), true)
 
 MY_LOCAL_PATH := $(call my-dir)
 
-ifneq ($(wildcard vendor/qcom/proprietary/args),)
 include $(MY_LOCAL_PATH)/hal/Android.mk
 include $(MY_LOCAL_PATH)/hal/audio_extn/Android.mk
 include $(MY_LOCAL_PATH)/audio-effects/Android.mk
-endif
 
 endif
 endif
