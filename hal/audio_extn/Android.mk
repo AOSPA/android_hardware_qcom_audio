8650630d391da53acd60c51e4e22066946e1f96e
Raghu Ballappa Bankapur <quic_rbankapu@quicinc.com>
2024-07-30 20:15:03 +0530

    audio-hal: Dont compile BATTERY LISTENER for Auto Target
    Change-Id: Ia599eba46291b9289932ed3cd3b19ab5a86d7fd8
    Signed-off-by: Raghu Ballappa Bankapur <quic_rbankapu@quicinc.com>

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -904,6 +904,7 @@ LOCAL_SANITIZE := integer_overflow
 endif
 include $(BUILD_SHARED_LIBRARY)
 
+ifneq ($(TARGET_BOARD_AUTO),true)
 #-------------------------------------------
 #            Build BATTERY_LISTENER
 #-------------------------------------------
@@ -974,6 +975,7 @@ ifneq ($(filter kona lahaina holi,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_SANITIZE := integer_overflow
 endif
 include $(BUILD_SHARED_LIBRARY)
+endif
 
 #-------------------------------------------
 #            Build HWDEP_CAL
