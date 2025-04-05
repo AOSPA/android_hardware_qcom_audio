7e19aa406c85bdcd7cf65b2617466f0c5dde5b15
Omkar Chandorkar <gotenksIN@aospa.co>
2025-04-05 09:11:20 +0530

    audio: Allow building audio-effects
    Change-Id: I2d8d85a27c81ad94cb46b3ce8bae225495d24142
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- Android.mk
+++ Android.mk
@@ -6,8 +6,8 @@ MY_LOCAL_PATH := $(call my-dir)
 ifneq ($(wildcard vendor/qcom/proprietary/args),)
 include $(MY_LOCAL_PATH)/hal/Android.mk
 include $(MY_LOCAL_PATH)/hal/audio_extn/Android.mk
-include $(MY_LOCAL_PATH)/audio-effects/Android.mk
 endif
+include $(MY_LOCAL_PATH)/audio-effects/Android.mk
 
 endif
 endif
